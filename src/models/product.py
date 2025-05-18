f950b8e96aa5821a4821793b348df1763b10c5f7
v.mishurenko <paranoiawe@gmail.com>
2025-05-18 20:17:06 +0300

    create cart, product models

---
+++ src/models/product.py
@@ -0,0 +1,27 @@
+from sqlalchemy import String, Integer, Boolean, func, TIMESTAMP, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.models.base import Base
+
+
+
+class Product(Base):
+
+    __tablename__ = "products"
+
+    name: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    price: Mapped[int] = mapped_column(Integer, nullable=False)
+    created_at: Mapped[DateTime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=func.now()
+    )
+    updated_at: Mapped[DateTime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        onupdate=func.now()
+    )
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+
+    cart_items = relationship("CartItem", back_populates="product")
--- src/models/user.py
+++ src/models/user.py
@@ -3,7 +3,7 @@ from typing import TYPE_CHECKING
 from fastapi_users.db import SQLAlchemyBaseUserTable
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.models.base import Base
 
@@ -17,8 +17,10 @@ class User(Base, SQLAlchemyBaseUserTable[int]):
 
     full_name: Mapped[str] = mapped_column(String(255), nullable=False)
     phone: Mapped[str] = mapped_column(
-        String(15), unique=True, index=True, nullable=False
+        String(12), unique=True, index=True, nullable=False
     )
+
+    cart = relationship("Cart", back_populates="user", uselist=False, cascade="all, delete-orphan")
 
     @classmethod
     def get_db(cls, session: "AsyncSession"):
